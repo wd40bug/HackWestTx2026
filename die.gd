542ded902de8a60d24535cad849e81e95c0287e0
jgillins <jeremygillins@icloud.com>
2026-09-12 20:22:05 -0500

    Functional game!

--- die.gd
+++ die.gd
@@ -3,16 +3,24 @@ extends Node
 
 var die_sides: Array[Die_side] = []
 var die_ability: Novilty_ability
+var die_index: int
+
 enum Parameter {WEIGHT, ABILITY, NUM}
 enum Novilty_ability {NONE}
 
-# -1 = not rolling
 # 0 = rolling
 # 1-6 is side to show
+# 7 is unclickable
+# 8 is transfer from unclickable to clickable
 var die_state: int = 1
 
 signal die_roll_state(state: int)
+signal clicked_signal(state: bool, index: int)
+
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var button = $AnimatedSprite2D/Button
+
+var clickable: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -24,10 +32,22 @@ func _process(delta: float) -> void:
 		return
 	$AnimatedSprite2D.animation = "Roll"
 	if(die_state == 0):
+		clickable = false
 		$AnimatedSprite2D.play()
+	elif(die_state == 7):
+		clickable = false
+		animated_sprite.modulate = Color(0, 0, 0, 0)
+	elif(die_state == 8):
+		clickable = false
+		animated_sprite.modulate = Color(1, 1, 1, 1)
+		die_state = 1
+		print("Die index: ", die_index)
+		print("Die state: ", die_state)
 	else:
+		clickable = true
 		$AnimatedSprite2D.stop()
 		$AnimatedSprite2D.frame = die_state - 1
+	
 
 func create_sides(die_index: int):
 	for i in range(0, 6):
@@ -71,6 +91,7 @@ func roll_die():
 	var random_number: float = randf_range(0.0, total_weight)
 	var acc_weight: float = 0.0
 	
+	
 	for side in die_sides:
 		acc_weight += side.side_weight
 		if random_number < acc_weight:
@@ -78,4 +99,12 @@ func roll_die():
 			emit_signal("die_roll_state", die_state)
 			return side
 	
-		
+func _on_button_toggled(toggled_on: bool) -> void:
+	if(toggled_on and die_state != 0 and clickable):
+		animated_sprite.modulate = Color(10, 10, 10, 1)
+		emit_signal("clicked_signal", toggled_on, die_index)
+	elif(die_state != 7):
+		animated_sprite.modulate = Color(1, 1, 1, 1)
+		emit_signal("clicked_signal", toggled_on, die_index)
+	elif(die_state == 7):
+		emit_signal("clicked_signal", toggled_on, die_index)
